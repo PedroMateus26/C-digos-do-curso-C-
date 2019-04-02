e7412178affc4e96673d71eb255b480e37a722c8
Pedro Mateus <pedromateusneto@gmail.com>
2019-04-02 18:00:10 -0300

    coloquei uma linha

--- TEsteGit/Program.cs
+++ TEsteGit/Program.cs
@@ -7,6 +7,7 @@ namespace TEsteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine( "oioioii");
         }
     }
 }
